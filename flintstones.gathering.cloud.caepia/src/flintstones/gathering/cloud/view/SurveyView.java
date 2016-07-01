0959100f5772b212a97efac0f9fae7741db56fbe
Álvaro Labella Romero <alabella@ujaen.es>
2016-07-01 17:16:52 +0200

    Subiendo plugin nebula.nattable

--- flintstones.gathering.cloud.caepia/src/flintstones/gathering/cloud/view/SurveyView.java
+++ flintstones.gathering.cloud.caepia/src/flintstones/gathering/cloud/view/SurveyView.java
@@ -1,8 +1,19 @@
 package flintstones.gathering.cloud.view;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
+import net.sourceforge.nattable.NatTable;
+import net.sourceforge.nattable.data.IColumnPropertyAccessor;
+import net.sourceforge.nattable.data.IDataProvider;
+import net.sourceforge.nattable.data.ListDataProvider;
+import net.sourceforge.nattable.data.ReflectiveColumnPropertyAccessor;
+import net.sourceforge.nattable.layer.DataLayer;
 
 public class SurveyView extends ViewPart {
 
@@ -10,16 +21,26 @@ public class SurveyView extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
+		String[] propertyNames = { "firstName", "lastName", "gender", "married", "birthday" };
+
+		List<String> examples = new LinkedList<String>();
+
+		IColumnPropertyAccessor<String> columnPropertyAccessor = new ReflectiveColumnPropertyAccessor<String>(propertyNames);
+		IDataProvider bodyDataProvider = new ListDataProvider<String>(examples, columnPropertyAccessor);
+		DataLayer bodyDataLayer = new DataLayer(bodyDataProvider);
 		
+		NatTable natTable = new NatTable(parent, SWT.NO_REDRAW_RESIZE | SWT.DOUBLE_BUFFERED | SWT.BORDER, bodyDataLayer);
+
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(natTable);
 	}
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	public void refresh() {}
+	public void refresh() {
+	}
 
 }
